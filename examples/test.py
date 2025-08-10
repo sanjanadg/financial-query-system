d1c40586932b7b7d1900a1ad6b667880e5c2fc2c
Sanjana D <sanjanadgup@gmail.com>
2025-08-10 15:40:46 -0400

    path fix

--- examples/test.py
+++ examples/test.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import process_file, excel_query
 
 # Process an Excel file
@@ -5,6 +8,6 @@ representation = process_file("data/Consolidated Plan 2023-2024.xlsm")
 print(f"Processed {len(representation)} sheets")
 
 # Query the data
-answer = excel_query("What is MXD's Gross Profit in Jan 2022?", representation)
+answer = excel_query("What percent of MXDs costs are indirect? Which month had the highest percentage?", representation)
 print(answer)
 
